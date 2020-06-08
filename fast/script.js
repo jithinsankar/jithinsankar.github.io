60d4d1125497bfffb1e7d32c67b7cc7c91a9f3eb
jithin Sankar <jithinsankar.nk@gmail.com>
2020-06-09 00:19:39 +0530

    added fast

---
+++ fast/script.js
@@ -0,0 +1,33 @@
+function live() {
+  var site = document.getElementById("site").value;
+    if (site!="")
+      {
+        document.getElementById("nmap").innerHTML ="nmap -sV -sT -sC -o nmapinitial " + site
+        document.getElementById("sql").innerHTML ="sqlmap -u "+ site +" --dbs";
+        document.getElementById("gobuster").innerHTML = "gobuster dir -u  "+site+" -w /usr/share/wordlists/dirb/common.txt -x php"
+      }
+    else{
+      document.getElementById("nmap").innerHTML = ""
+      document.getElementById("sql").innerHTML =""
+      document.getElementById("gobuster").innerHTML = ""
+    }
+  }
+
+function copyToClipboard(text) {
+    var dummy = document.createElement("textarea");
+    // to avoid breaking orgain page when copying more words
+    // cant copy when adding below this code
+    // dummy.style.display = 'none'
+    document.body.appendChild(dummy);
+    //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
+    dummy.value = text;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+}
+function copyText(id){
+copyToClipboard(document.getElementById(id).innerHTML)
+  document.getElementById(id).style.fontsize="60px";
+
+  console.log(document.getElementById(id).style.fontsize)
+}
